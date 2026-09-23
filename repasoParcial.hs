3e60abd39d271b5fdd6ffc73c940e04ae6e3019d
Rafael Alvez <rafialvez@gmail.com>
2026-09-23 15:40:05 -0300

    ejercicios 2 y 5 hechos

--- repasoParcial.hs
+++ repasoParcial.hs
@@ -7,3 +7,22 @@ sumaTotal ((fila):matriz) = sumatoria (fila) + sumaTotal (matriz)
 sumatoria :: [Integer] -> Integer
 sumatoria [] = 0
 sumatoria (x:xs) = x + sumatoria (xs)
+
+--2.
+cantidadDeApariciones :: Integer -> [[Integer]] -> Integer
+cantidadDeApariciones k [] = 0
+cantidadDeApariciones k ((fila):matriz) = aparicionesEnFila k (fila) + cantidadDeApariciones k (matriz)
+
+aparicionesEnFila :: Integer -> [Integer] -> Integer
+aparicionesEnFila k [] = 0
+aparicionesEnFila k (x:xs) |k==x = 1 + aparicionesEnFila k (xs)
+                           |otherwise = aparicionesEnFila k (xs)
+
+--5.
+multiplicarPorEscalar :: Integer -> [[Integer]] -> [[Integer]]
+multiplicarPorEscalar k [] = []
+multiplicarPorEscalar k ((fila):matriz) = [multiplicarFila k (fila)] ++ multiplicarPorEscalar k (matriz)
+
+multiplicarFila :: Integer -> [Integer] -> [Integer]
+multiplicarFila k [] = []
+multiplicarFila k (x:xs) = [k*x] ++ multiplicarFila k (xs)

defb8b1861df08910317f147f83a08e9e79d7c68
Rafael Alvez <rafialvez@gmail.com>
2026-09-23 15:23:44 -0300

    ejercicio 1 hecho

--- repasoParcial.hs
+++ repasoParcial.hs
@@ -1,1 +1,9 @@
---hola esto es de prueba
+--ejercicio 8
+
+sumaTotal :: [[Integer]] -> Integer
+sumaTotal [] = 0
+sumaTotal ((fila):matriz) = sumatoria (fila) + sumaTotal (matriz)
+
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria (xs)
